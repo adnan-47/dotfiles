fc6f42c4a194b25a84b613aaa0c15c4478a57333
Adnan <adnanofficial47@gmail.com>
2025-05-04 17:26:25 +0530

    Create omp-confprofile.ps1

---
+++ omp-confprofile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\1_shell.omp.json" | Invoke-Expression
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force    
+Import-Module -Name Terminal-Icons
+
+#omp profile setup
+#1_shell.opm is the theame 
+# import icons 
